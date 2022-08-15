715134bda2fdf7694a48703aa36f1ede8ce3b200
arafcse <arafahmed.yt@gmail.com>
2022-08-15 18:11:56 +0600

    Botman error

--- app/Http/Controllers/BotmanController.php
+++ app/Http/Controllers/BotmanController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
 use BotMan\BotMan\Messages\Incoming\Answer;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class BotManController extends Controller
 {
@@ -13,35 +15,33 @@ class BotManController extends Controller
      */
     public function handle()
     {
-
          $botman = app('botman');
          $botman->hears('{message}', function($botman, $message) {
-         if ($message == 'hi')
+         if ($message == 'hello')
           {
             $this->askName($botman);
           }
           else
           {
-            $botman->reply("write 'hi' for testing...");
+            $botman->reply("say 'hello' for start chatting..");
           }
        });
-
        $botman->listen();
-
     }
-
-      /**
-       * Place your BotMan logic here.
-      */
-
       public function askName($botman)
       {
-
-          $botman->ask('Hello! What is your Name?', function(Answer $answer) {
-          $name = $answer->getText();
-
-          $this->say('Nice to meet you '.$name);
+          $botman->ask('Hello! Who are you looking for?', function(Answer $answer) {
+          //$name = $answer->getText();
+          $results = $answer->getText();
+          $results = DB::table('users')
+            ->where('name','LIKE','%'.$results.'%')
+            ->get();
+          if(count($results)>0){
+              $botman->reply('I found a member for '.$results);
+          }
+          else{
+              $botman->reply('Sorry! Member not found for'.$results);
+              // send report
+          }
         });
-
       }
-}
--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,5 @@ class User extends Authenticatable
     public function receivesBroadcasNotificationsOn(){
         return 'post_like'. $this->id;
     }
+
 }
